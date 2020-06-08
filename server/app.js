c58b87c12f12be4923552045f86bdb72a78c6cec
Michael Tamsil <michaeltamsil@gmail.com>
2020-06-08 12:42:24 +0700

    have issue with on admin point

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+
 var cors = require('cors')
 var createError = require('http-errors');
 var express = require('express');
@@ -8,9 +9,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+require('dotenv').config({
+  path: path.resolve(__dirname, './.env')
+});
 
-session = {}
-
+var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,8 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(session({
-  cookie: {},
+
   resave: false,
   saveUninitialized: false,
   secret: process.env.SESSION_SECRET
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { OAuth2Client } = require('google-auth-library')
 const client = new OAuth2Client(process.env.GOOGLE_OAUTH2_CLIENT_ID)
+console.log(process.env.GOOGLE_OAUTH2_CLIENT_ID)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,15 +17,32 @@ router.post('/', async function(req, res, next){
   })
 
   const payload = ticket.getPayload();
-  const userid = payload['sub']; //google id, but we can use email
-  const email = payload['email']; // email
+  //const userid = payload['sub']; //google id, but we can use email
+  //const email = payload['email']; // email
+  const { userid, email, name } = payload;
+
 
   req.session.email = email;
   req.session.name = name
-  console.log(payload)
-  res.json({
-    "message" : "wow"
-  })
+  console.log(req.session)
+  //console.log(payload)
+  res.json(
+    {
+      userid,
+      email,
+      name
+    }
+  )
+})
+
+router.get('/admin', (req, res)=>{
+  console.log(req.session)
+  if(req.session.email){
+    res.send("ok");
+  }else{
+    res.status(403)
+    res.send("not allowed");
+  }
 })
 
 router.get('/logout', function(req, res){
